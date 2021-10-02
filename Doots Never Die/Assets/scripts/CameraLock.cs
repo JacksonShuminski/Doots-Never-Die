33e4b546eee0a4b9996c8ae966d9bec67bfdc583
Tanuki3017 <57275147+Tanuki3017@users.noreply.github.com>
2021-10-02 18:51:36 -0400

    Added a script that allows the camera to follow the player

---
+++ Doots Never Die/Assets/scripts/CameraLock.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    public Transform target;
+    public float smoothness; //Time it takes for the camera to move
+    private Vector3 velocity;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(0, 0, -10); //Set to when the player starts to go off screen
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothness);
+    }
+}
