84f3715014b1c9edbe57dce83b6b7a3f96e047d9
Tanuki3017 <57275147+Tanuki3017@users.noreply.github.com>
2021-10-02 18:01:27 -0400

    Player now moves

---
+++ Doots Never Die/Assets/scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Code for movement
+    private Rigidbody2D rigidbody;
+    private Vector3 currentPosition; //Position of the player
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = transform.position;
+        Vector3 moveAmount = Vector3.zero;
+
+        //Movement
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveAmount = new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveAmount = new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveAmount = new Vector3(0, 1, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveAmount = new Vector3(0, -1, 0);
+        }
+
+        moveAmount = moveAmount.normalized;
+        rigidbody.MovePosition(currentPosition + moveAmount);
+
+    }
+}
