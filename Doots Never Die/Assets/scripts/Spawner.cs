d7d4b7549032f898f9583a42c4d8efa76d3ec419
Siris-Alex <56982691+Siris-Alex@users.noreply.github.com>
2021-10-28 14:33:30 -0400

    Spawner should work but doesn't

--- Doots Never Die/Assets/scripts/Spawner.cs
+++ Doots Never Die/Assets/scripts/Spawner.cs
@@ -7,6 +7,7 @@ public class Spawner : MonoBehaviour
     public List<GameObject> children;
     public GameObject child;
     public List<GameObject> spawnPoints;
+    public Camera camera;
     int timer;
     int difficulty;
     // Start is called before the first frame update
@@ -20,9 +21,12 @@ public class Spawner : MonoBehaviour
         if(children.Count <= 100 && timer >= 180)
         {
             int randomSpawn = Random.Range(0, 9);
-            GameObject spawn = Instantiate(child, spawnPoints[randomSpawn].transform.position, Quaternion.identity);
-            children.Add(spawn);
-            timer = 0;
+            if (!camera.rect.Contains(spawnPoints[randomSpawn].transform.position))
+            {
+                GameObject spawn = Instantiate(child, spawnPoints[randomSpawn].transform.position, Quaternion.identity);
+                children.Add(spawn);
+                timer = 0;
+            }
         }
         timer++;
     }
