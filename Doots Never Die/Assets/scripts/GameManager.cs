8b46363651d54751ccad0d1f74dfc9b482bac054
DeadBreadRedemption <ss4405@rit.edu>
2021-10-28 17:24:55 -0400

    Added pause and play function

--- Doots Never Die/Assets/scripts/GameManager.cs
+++ Doots Never Die/Assets/scripts/GameManager.cs
@@ -3,44 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum GameState
-{
-    Menu,
-    Play,
-    Pause,
-    End
-}
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameState state;
 
     void Start()
     {
-        DontDestroyOnLoad(transform.gameObject);
-        state = GameState.Menu;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (state == GameState.Menu && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("Game");
-            state = GameState.Play;
-        }
-
-        if (state == GameState.Play &&  Input.GetKeyDown(KeyCode.P))
-        {
-            SceneManager.LoadScene("SaumilScene");
-            state = GameState.Pause;
-        }
-
-        if(state == GameState.Pause && Input.GetKeyDown(KeyCode.P))
-        {
-            SceneManager.LoadScene("Game");
-            state = GameState.Play;
         }
     }
 }
