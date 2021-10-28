00c54900053a38f7ff1726db20a7bd33e799fa62
Jackson <jvs8230@rit.edu>
2021-10-27 20:53:51 -0400

    It is MERGED!!!!!

--- Doots Never Die/Assets/shaders/postprocess.cs
+++ Doots Never Die/Assets/shaders/postprocess.cs
@@ -7,15 +7,20 @@ public class postprocess : MonoBehaviour
 {
     public Material mat;
     public Player player;
+    public AOE aoe_script;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        float wiggle = (1-Mathf.Min(player.timer / player.maxTime, 1)) * 25;
+        float wiggle = (1 - Mathf.Min(player.timer / player.maxTime, 1)) * 15;
 
         mat.SetFloat("_Width", source.width);
         mat.SetFloat("_Height", source.height);
         mat.SetFloat("_WiggleFactor", wiggle);
 
+        if (aoe_script.maxTime - aoe_script.timer < 1 && aoe_script.maxTime != aoe_script.timer)
+            mat.SetFloat("_Scream", (aoe_script.maxTime - aoe_script.timer));
+        else
+            mat.SetFloat("_Scream", 1);
 
         Graphics.Blit(source, destination, mat);
     }
